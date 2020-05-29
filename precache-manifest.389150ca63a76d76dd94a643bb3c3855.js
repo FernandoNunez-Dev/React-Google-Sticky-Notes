0ae9a80d331156a9733a229dfa466a09252d186a
telly <telly@Dev3.local>
2020-05-29 06:12:57 -0400

    Updates

---
+++ precache-manifest.389150ca63a76d76dd94a643bb3c3855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a10b93c606aa4c43af76b939771b2a3",
+    "url": "/react-google-sticky-notes/index.html"
+  },
+  {
+    "revision": "fff41f83e84209395539",
+    "url": "/react-google-sticky-notes/static/js/2.072feeda.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/react-google-sticky-notes/static/js/2.072feeda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3d9eaf60fa43f4f20e",
+    "url": "/react-google-sticky-notes/static/js/main.ee66ddf4.chunk.js"
+  },
+  {
+    "revision": "de98074fd11f65348de1",
+    "url": "/react-google-sticky-notes/static/js/runtime-main.d698e855.js"
+  }
+]);
